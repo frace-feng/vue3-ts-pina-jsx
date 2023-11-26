99c7b530c69a11986f021b8c92e206aac628adb3
FRACE <frace@inmoods.com>
2023-11-26 22:49:23 +0800

    husky

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -1,12 +1,17 @@
-import { defineComponent, reactive, toRefs,toRef } from "vue";
-import {ITodoItem,useTodoListStore} from './TodoList';
+import { defineComponent, reactive, toRefs, toRef } from "vue";
+import { ITodoItem, useTodoListStore } from "./TodoList";
 
 interface itemProps {
   item: ITodoItem;
 }
 export const TodoItem = defineComponent({
-  props: ["item"],
-  setup(props:itemProps) {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  setup(props: itemProps) {
     const store = useTodoListStore();
     const state = reactive({ isEdit: false, itemVal: "" });
     // 将 `props` 转为一个其中全是 ref 的对象，然后解构
@@ -29,8 +34,13 @@ export const TodoItem = defineComponent({
         ) : (
           <div>
             {name.value}
-            <button style={{margin:'0 10px'}} onClick={()=>state.isEdit = true}>编辑</button>
-            <button onClick={()=>store.del(id)}>删除</button>
+            <button
+              style={{ margin: "0 10px" }}
+              onClick={() => (state.isEdit = true)}
+            >
+              编辑
+            </button>
+            <button onClick={() => store.del(id)}>删除</button>
           </div>
         )}
       </div>
